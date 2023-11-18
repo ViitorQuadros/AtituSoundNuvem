338e6f23465a12e7c6fee557892e52ff660cadd1
Vitor Quadros <vitoralgusto452@gmail.com>
2023-11-18 19:15:42 -0300

    Aula 12 funcional

---
+++ atitusound/src/main/java/br/edu/atitus/poo/atitusound/controllers/ArtistController.java
@@ -0,0 +1,98 @@
+package br.edu.atitus.poo.atitusound.controllers;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.swing.text.html.parser.Entity;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.atitus.poo.atitusound.dtos.ArtistDTO;
+import br.edu.atitus.poo.atitusound.entities.ArtistEntity;
+import br.edu.atitus.poo.atitusound.service.ArtistService;
+import br.edu.atitus.poo.atitusound.serviceimp.ArtistServiceImpl;
+
+@RestController
+@RequestMapping("/artists")
+public class ArtistController {
+
+	
+	private final ArtistService service;
+	public ArtistController(ArtistService service) {
+		super();
+		this.service = service;
+	}
+
+
+	protected ArtistEntity convertDTO2Entity(ArtistDTO dto) {
+		ArtistEntity entity = new ArtistEntity();
+		entity.setName(dto.getName());
+		entity.setImage(dto.getImage());
+		return entity;
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<ArtistEntity> postSave(@RequestBody ArtistDTO dto){
+		ArtistEntity entity = convertDTO2Entity(dto);
+		try {
+			service.save(entity);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().header("error",e.getMessage()).build();
+		}
+		return ResponseEntity.status(HttpStatus.CREATED).body(entity);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<List<ArtistEntity>>> getFind(
+			@PageableDefault(page = 0, size = 10, sort = "name", direction = Direction.ASC) Pageable pageable, @RequestParam String name){
+		Page<List<ArtistEntity>> lista;
+		try {
+			lista = service.findByNameContainingIgnoreCase(name, pageable);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().header("error",e.getMessage()).build();
+		}
+		return ResponseEntity.ok(lista);
+	}
+	
+	@GetMapping("/{uuid}")
+	public ResponseEntity<ArtistEntity> getFindById(@PathVariable UUID uuid){
+		Optional<ArtistEntity> entity;
+		try {
+			 entity = service.findById(uuid);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().header("error",e.getMessage()).build();
+		}
+		if(entity.isEmpty())
+			return ResponseEntity.notFound().build();
+		else
+			return ResponseEntity.ok(entity.get());
+	}
+	
+	@PutMapping("/{uuid}")
+	public ResponseEntity<ArtistEntity> put(@PathVariable UUID uuid, @RequestBody ArtistDTO dto){
+		ArtistEntity entity = convertDTO2Entity(dto);
+		entity.setUuid(uuid);
+		try {
+			service.save(entity);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().header("error", e.getMessage()).build();
+		}
+		return ResponseEntity.ok(entity);
+	}
+	
+}
---
+++ atitusound/src/main/java/br/edu/atitus/poo/atitusound/serviceimp/ArtistServiceImpl.java
@@ -0,0 +1,57 @@
+package br.edu.atitus.poo.atitusound.serviceimp;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.edu.atitus.poo.atitusound.entities.ArtistEntity;
+import br.edu.atitus.poo.atitusound.repositories.ArtistRepository;
+import br.edu.atitus.poo.atitusound.service.ArtistService;
+
+@Service
+public class ArtistServiceImpl implements ArtistService{
+
+	private final ArtistRepository repository;
+	
+	public ArtistServiceImpl(ArtistRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+	protected void validade (ArtistEntity entity) throws Exception{
+		if (entity.getName() == null || entity.getName().isEmpty())
+			throw new Exception("Campo nome inválido");
+		if (repository.existsByName(entity.getName()))
+			throw new Exception("Já existe registro com esse nome");
+	}
+	
+	@Override
+	public ArtistEntity save(ArtistEntity entity) throws Exception {
+		validade(entity);
+		repository.save(entity);
+		return entity;
+	}
+
+	@Override
+	public List<ArtistEntity> findAll() throws Exception {
+	
+		return repository.findAll();
+	}
+
+	@Override
+	public Page<List<ArtistEntity>> findByNameContainingIgnoreCase(String name, Pageable pageable) throws Exception {
+		
+		return repository.findByNameContainingIgnoreCase(name, pageable);
+	}
+
+	@Override
+	public Optional<ArtistEntity> findById(UUID uuid) throws Exception {
+		
+		return repository.findById(uuid);
+	}
+
+}
