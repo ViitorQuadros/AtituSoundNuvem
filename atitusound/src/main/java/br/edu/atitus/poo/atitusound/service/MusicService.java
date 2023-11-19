2be00b41d31729cf621dc51d85e1f4bcccc2a062
Vitor Quadros <vitoralgusto452@gmail.com>
2023-11-19 11:46:21 -0300

    AULA 18 OK

---
+++ atitusound/src/main/java/br/edu/atitus/poo/atitusound/service/MusicService.java
@@ -0,0 +1,7 @@
+package br.edu.atitus.poo.atitusound.service;
+
+import br.edu.atitus.poo.atitusound.entities.MusicEntity;
+
+public interface MusicService extends GenericService<MusicEntity>{
+	
+}
