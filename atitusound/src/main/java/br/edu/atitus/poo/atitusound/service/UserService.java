5169b32035882fc7adc250377f9eee86561280f9
Vitor Quadros <vitoralgusto452@gmail.com>
2023-11-19 20:35:55 -0300

    AULA 23 OK

--- atitusound/src/main/java/br/edu/atitus/poo/atitusound/service/UserService.java
+++ atitusound/src/main/java/br/edu/atitus/poo/atitusound/service/UserService.java
@@ -1,9 +1,11 @@
 package br.edu.atitus.poo.atitusound.service;
 
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import br.edu.atitus.poo.atitusound.entities.UserEntity;
 
-public interface UserService extends GenericService<UserEntity>{
+public interface UserService extends GenericService<UserEntity>, UserDetailsService{
 	
 	
 	
