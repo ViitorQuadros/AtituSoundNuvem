17ec72853f8accf033a7d7f2971472154395e69d
Vitor Quadros <vitoralgusto452@gmail.com>
2023-11-20 19:49:20 -0300

    AULA 24 OK

---
+++ atitusound/src/main/java/br/edu/atitus/poo/atitusound/service/PlaylistService.java
@@ -0,0 +1,9 @@
+package br.edu.atitus.poo.atitusound.service;
+
+import br.edu.atitus.poo.atitusound.entities.PlaylistEntity;
+
+public interface PlaylistService extends GenericService<PlaylistEntity> {
+
+	
+	
+}
